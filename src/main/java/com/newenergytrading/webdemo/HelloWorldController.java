05475d4009c7be245cab59e2bff73983fe2af18b
Your Name <you@example.com>
2021-11-02 11:43:59 +0100

    code updated.

--- src/main/java/com/newenergytrading/webdemo/HelloWorldController.java
+++ src/main/java/com/newenergytrading/webdemo/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.newenergytrading.webdemo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public ResponseEntity<String> sayHello(@RequestParam String firstname, String lastname) {
+    public ResponseEntity<String> sayHello(@RequestParam String firstname,@RequestParam String lastname) {
         return ResponseEntity.ok("Hello " + firstname + lastname + "!");
     }
 
     @GetMapping("thymeleaf")
-    public String sayHelloThymeleaf() {
+    public String sayHelloThymeleaf(Model model,@RequestParam String firstname,@RequestParam String lastname) {
+        model.addAttribute("name", firstname + " " + lastname);
+        model.addAttribute("loremipsum", "Gib Gas");
         return "hello-template";
     }
+
 }
